c575e54893803d640be8af9c311f46c1788820e5
Hasan9955 <hasanbinali7556@gmail.com>
2023-09-15 11:47:45 +0600

    'second'

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Blogs from './compnents/Blogs/Blogs'
+import Header from './compnents/Header/Header'
+
+function App() {
+
+
+  return (
+    <>
+      
+      
+      <Header></Header>
+      <Blogs></Blogs>
+    </>
+  )
+}
+
+export default App
